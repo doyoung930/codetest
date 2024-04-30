dd44a2d33aadd6bec88398aac042352a3ec5a37f
doyoung930 <70666642+doyoung930@users.noreply.github.com>
2024-04-30 14:19:44 +0900

    달리기 경주

--- Code_Test/Code_Test/main.cpp
+++ Code_Test/Code_Test/main.cpp
@@ -1,30 +1,55 @@
 #include "header.h"
 
-#include "달리기경주.cpp"
-using namespace std;
 
 
-// 각 시간과 시간과 관련된 로직을 가지는 Time 클래스
+//vector<string> solution(vector<string> players, vector<string> callings)
+//{
+//    vector<string> answer;
+//    map<int, string> m1;
+//    map<string, int> m2;
+//    for (int i = 0; i < players.size(); i++)
+//    {
+//        m1[i] = players[i];
+//        m2[players[i]] = i;
+//    }
+//    for (int i = 0; i < callings.size(); i++)
+//    {
+//        int cur_idx = m2[callings[i]];//호출 선수 이름, 순위
+//        string change = m1[cur_idx - 1];//앞선 선수 이름, 순위
+//        m1[cur_idx - 1] = callings[i];
+//        m1[cur_idx] = change;
+//        m2[callings[i]] = cur_idx - 1;
+//        m2[change] = cur_idx;
+//    }
+//    for (auto c : m1) answer.push_back(c.second);
+//    return answer;
+//}
+
 vector<string> solution(vector<string> players, vector<string> callings) {
     vector<string> answer;
-    vector<string> temp;
 
-    for (auto& c : callings) {
-        for (int i = 0; i < size(answer); i++) {
-            if (answer[i] == c) {
-                if (i != 0) {
-                    temp[i - 1] = answer[i];
-                    temp[i] = answer[i - 1];
-                }
-                else
-                    temp[i] = answer[i];
-            }
-            else
-                temp[i] = answer[i];
+    map<int, string> ranking_name;
+    map<string, int> name_ranking;
 
-        }
-
+    for (int i = 0; i < players.size(); i++) {
+        ranking_name[i] = players[i];
+        name_ranking[players[i]] = i;
     }
+    for (int i = 0; i < callings.size(); i++)
+    {
+        int ranking = name_ranking[callings[i]]; // 부른선수
+        string name = ranking_name[ranking - 1]; // 앞에 선수 이름, 순위
+
+        ranking_name[ranking - 1] = callings[i];
+        ranking_name[ranking] = name;
+
+        name_ranking[callings[i]] = ranking - 1;
+        name_ranking[name] = ranking;
+    }
+
+    for (auto a : ranking_name)
+        answer.push_back(a.second);
+
 
     return answer;
 }
@@ -36,7 +61,7 @@ int main() {
 
     vector<string> answers;
     answers = solution(player, calling);
-    for (auto& t : answers) {
+    for (auto& t : solution(player, calling)) {
         std::cout << t << std::endl;
     }
     return 0;
--- Code_Test/Code_Test/달리기경주.cpp
+++ Code_Test/Code_Test/달리기경주.cpp
@@ -3,24 +3,29 @@
 
 vector<string> solution(vector<string> players, vector<string> callings) {
     vector<string> answer;
-    vector<string> temp;
-    
-    for (auto c : callings) {
-        for (int i = 0; i < size(answer); i++) {
-            if (answer[i] == c) {
-                if (i != 0) {
-                    temp[i - 1] = answer[i];
-                    temp[i] = answer[i - 1];
-                }
-                else
-                    temp[i] = answer[i];
-            }
-            else
-                temp[i] = answer[i];
-            
-        }
 
+    map<int, string> ranking_name;
+    map<string, int> name_ranking;
+
+    for (int i = 0; i < players.size(); i++) {
+        ranking_name[i] = players[i];
+        name_ranking[players[i]] = i;
     }
+    for (int i = 0; i < callings.size(); i++)
+    {
+        int ranking = name_ranking[callings[i]]; // 부른선수
+        string name = ranking_name[ranking - 1]; // 앞에 선수 이름, 순위
+
+        ranking_name[ranking - 1] = callings[i];
+        ranking_name[ranking] = name;
+
+        name_ranking[callings[i]] = ranking - 1;
+        name_ranking[name] = ranking;
+    }
+
+    for (auto a : ranking_name)
+        answer.push_back(a.second);
+
 
     return answer;
 }
