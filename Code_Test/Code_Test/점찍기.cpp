61132d993f6973012559e3532826916da8434db9
doyoung930 <doyoung930@gmail.com>
2025-01-24 01:34:59 +0900

    프로그래머스 점 찍기

---
+++ Code_Test/Code_Test/점찍기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+
+long long solution(int k, int d) {
+    long long answer = d / k + 1;
+
+    for (long long i = 0; i < d; i += k)
+    {
+        answer += (long long)sqrt(1LL * d * d - i * i) / k;
+    }
+
+
+    return answer;
+}
